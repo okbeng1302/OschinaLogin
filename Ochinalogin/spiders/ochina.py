b0251ec18aacc9ed85dd9516b821be6339228d21
okbeng <301070179@qq.com>
2017-04-21 21:41:45 +0800

    cookie 模拟登录 爬取全站

--- Ochinalogin/spiders/ochina.py
+++ Ochinalogin/spiders/ochina.py
@@ -2,7 +2,7 @@
 import scrapy
 import logging
 
-
+from scrapy.selector import Selector
 class OchinaSpider(scrapy.Spider):
     name = "ochina"
     allowed_domains = ["www.oschina.net"]
@@ -29,8 +29,20 @@ class OchinaSpider(scrapy.Spider):
     	return [scrapy.Request(url='https://www.oschina.net',method='get',cookies={'_user_behavior_':'4761e11b-abaf-49a9-9132-24bf449a9f0f','oscid':'4PpfWJFgaYBL0rdop%2FHhmei3BNm2DDV1PeX2CQ8wzefpkCHC1LShVbEBQM%2BsLM0jwJTJpqrzKXECzrVkxTmRBlejF4Rg8S0qaQpLqzUHyaD19r4smcHzru33MS1PWtqx79QspDmM3RARDd71PWXvza%2FSGWadDdrD3QXcLhN3ruAoTcIjSO%2FSgg%3D%3D','Hm_lvt_a411c4d1664dd70048ee98afe7b28f0b':'1492580323,1492666551,1492765230,1492765477','Hm_lpvt_a411c4d1664dd70048ee98afe7b28f0b':'1492777346'},headers=self.headers,callback=self.parse_one,dont_filter=True)]
     
     def parse_one(self,response):
+    	sel = Selector(response)
+    	title = sel.xpath('//title/text()').extract()[0]
+    	count = 0
+    	count = count + 1
+    	links = sel.xpath('.//a/@href').extract()
+    	for link in links:
+    		# print link
+    		if not link.startswith('https://'):
+    			if not link.startswith('http://'):
+    				link = 'https://www.oschina.net' + link
+    		yield scrapy.Request(url=link,callback=self.parse_one,headers=self.headers)
+
     	print response.url + '3'
-     	open('e:\hanhan\\ochina.txt', 'w+').write(response.body)
+     	open('e:\hanhan\\'+ title + '.txt', 'w+').write(response.body)
      	# logging.log(logging.WARNING,response.body)
     	# return [scrapy.Request(url='https://www.oschina.net/home/login',headers=response.headers,meta={'cookiejar':2},callback=self.parse_login, dont_filter=True)]
 
